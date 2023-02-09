f831b2f3b83adab0ec2ca31e396e74dbb81b378a
canyon <1225426443@qq.com>
2023-02-09 10:29:09 +0800

    try

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -3,7 +3,7 @@
  * @Author: jianguo
  * @Date: 2023-02-07 14:12:05
  * @LastEditors: jianguo
- * @LastEditTime: 2023-02-08 22:11:10
+ * @LastEditTime: 2023-02-09 10:29:05
  */
 import Header from "components/Header";
 import Head from "next/head";
@@ -15,7 +15,7 @@ export default function blog() {
                 <title>&apos;s Blog</title>
             </Head>
             <Header>
-                <div>111</div>
+                <div>11</div>
             </Header>
         </div>
         
